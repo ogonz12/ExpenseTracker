57e5692c33fe0fd250ecb5e979797bfddf9f8288
Orlando Gonzalez <orlandogonzz0412@gmail.com>
2024-07-03 10:56:29 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Orlando Gonzalez on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
