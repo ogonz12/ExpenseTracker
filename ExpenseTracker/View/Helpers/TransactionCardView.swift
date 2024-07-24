c55583b33dbe28a5da88ed53e9cd6a00a9ba3ed5
adaydev <adaydev@icloud.com>
2024-07-24 10:18:45 -0400

    Delete transaction added

--- ExpenseTracker/View/Helpers/TransactionCardView.swift
+++ ExpenseTracker/View/Helpers/TransactionCardView.swift
@@ -7,46 +7,52 @@
 
 import SwiftUI
 import SwiftData
+import WidgetKit
 
 struct TransactionCardView: View {
     @Environment(\.modelContext) private var context
     var transaction: Transaction
     
     var body: some View {
-        HStack {
-            Text("\(String(transaction.title.prefix(1)))")
-                .font(.title)
-                .fontWeight(.semibold)
-                .foregroundStyle(.white)
-                .frame(width: 45, height: 45)
-                .background(transaction.color.gradient, in: .circle)
-            
-            VStack(alignment: .leading, spacing: 4, content: {
-                Text(transaction.title)
-                    .foregroundStyle(Color.primary)
+        SwipeAction(cornerRadius: 10, direction: .trailing) {
+            HStack {
+                Text("\(String(transaction.title.prefix(1)))")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 45, height: 45)
+                    .background(transaction.color.gradient, in: .circle)
                 
-                Text(transaction.remarks)
-                    .font(.caption)
-                    .foregroundStyle(Color.primary.secondary)
+                VStack(alignment: .leading, spacing: 4, content: {
+                    Text(transaction.title)
+                        .foregroundStyle(Color.primary)
+                    
+                    Text(transaction.remarks)
+                        .font(.caption)
+                        .foregroundStyle(Color.primary.secondary)
+                    
+                    Text(format(date: transaction.dateAdded, format: "dd MMM yyyy"))
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                })
+                .lineLimit(1)
+                .frame(maxWidth: .infinity, alignment: .leading)
                 
-                Text(format(date: transaction.dateAdded, format: "dd MMM yyyy"))
-                    .font(.caption2)
-                    .foregroundStyle(.gray)
-            })
-            .lineLimit(1)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Text(currencyString(transaction.amount, allowedDigits: 2))
-                .fontWeight(.semibold)
+                Text(currencyString(transaction.amount, allowedDigits: 2))
+                    .fontWeight(.semibold)
+            } //HStack
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background(.background, in: .rect(cornerRadius: 10))
+        } actions: {
+            Action(tint: .red, icon: "trash") {
+                context.delete(transaction)
+                WidgetCenter.shared.reloadAllTimelines()
+            }
         }
-        .padding(.horizontal, 15)
-        .padding(.vertical, 10)
-        .background(.background, in: .rect(cornerRadius: 10))
-        } //HStack
-    
+    }
 }
 
 #Preview {
-    //TransactionCardView(transaction: Transaction(title: "Telephone", remarks: "T-Mobile", amount: 120, dateAdded: .now, category: .expense, tintColor: .init(color: "Red", value: .red)))
     HomeView()
 }
